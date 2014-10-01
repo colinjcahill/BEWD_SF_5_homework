73c62f4bd27bb425c7d5c1eed79c2893cd727383
Colin Cahill <colincahill@gmail.com>
2014-09-30 21:08:35 -0700

    User reference migration

--- ColinCahill-leroyskagnetti/Final Project/borrow/app/controllers/items_controller.rb
+++ ColinCahill-leroyskagnetti/Final Project/borrow/app/controllers/items_controller.rb
@@ -1,9 +1,13 @@
 class ItemsController < ApplicationController
+before_action :authenticate_user!, except: [:index, :show]
+
   def new
     @item = Item.new
   end
   def create
     @item = Item.create(params.require(:item).permit(:name, :description, :category, :value, :image_path, :tags, :condition))
+    @item.user_id = current_user.id
+    binding.pry
      if @item.save
       redirect_to @item, notice: "Item successfully created!"
     else
--- ColinCahill-leroyskagnetti/Final Project/borrow/db/schema.rb
+++ ColinCahill-leroyskagnetti/Final Project/borrow/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141001023231) do
+ActiveRecord::Schema.define(version: 20141001040758) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -20,19 +20,8 @@ ActiveRecord::Schema.define(version: 20141001023231) do
     t.datetime "updated_at"
   end
 
-  create_table "items", force: true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "value"
-    t.string   "image_path"
-    t.string   "tags"
-    t.integer  "condition"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "category_id"
-  end
-
-  add_index "items", ["category_id"], name: "index_items_on_category_id"
+# Could not dump table "items" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
